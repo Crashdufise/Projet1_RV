500586d48393a7901e320e902211d1ae78e961fc
LouisLN <Crashdufise@gmail.com>
2017-11-17 15:44:17 +0100

    Test 1

---
+++ test-camera_Sims/Assets/afficherCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class afficherCamera : MonoBehaviour {
+
+    public Camera surveillance;
+	bool isToShow;
+
+	// Use this for initialization
+	void Start () {
+        isToShow = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0));
+
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
+        {
+			surveillance.enabled = !surveillance.enabled;
+        }
+}
